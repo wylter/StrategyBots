e25b48adc980a8b87630c96fc1484d854de8327d
Davide Wylter Di Bernardo <dbr.davide@gmail.com>
2019-02-24 12:28:34 +0100

    Project setup

---
+++ Assets/TBS Framework/Scripts/Tutorial/SampleUnit.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+public class SampleUnit : Unit
+{
+    public Color LeadingColor;
+    public override void Initialize(){
+        base.Initialize();
+        transform.localPosition = new Vector3(transform.localPosition.x, 0.5f, transform.localPosition.z);
+        GetComponent<Renderer>().material.color = LeadingColor;
+    }
+    public override void MarkAsAttacking(Unit other)
+    {      
+    }
+
+    public override void MarkAsDefending(Unit other)
+    {       
+    }
+
+    public override void MarkAsDestroyed()
+    {      
+    }
+
+    public override void MarkAsFinished()
+    {
+    }
+
+    public override void MarkAsFriendly()
+    {
+        GetComponent<Renderer>().material.color = LeadingColor + new Color(0.8f, 1, 0.8f);
+    }
+
+    public override void MarkAsReachableEnemy()
+    {
+        GetComponent<Renderer>().material.color = LeadingColor + Color.red ;
+    }
+
+    public override void MarkAsSelected()
+    {
+        GetComponent<Renderer>().material.color = LeadingColor + Color.green;
+    }
+
+    public override void UnMark()
+    {
+        GetComponent<Renderer>().material.color = LeadingColor;
+    }
+}
