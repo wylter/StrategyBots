cc0e99d0312a01d81d09385dd7701fe872f48db1
Davide Di Bernardo <dbr.davide@gmail.com>
2019-03-26 20:40:46 +0100

    -pick fase inplemented

--- Assets/Scripts/TBS Framework Custom Extentions/CustomUnitGeneratorFromData.cs
+++ Assets/Scripts/TBS Framework Custom Extentions/CustomUnitGeneratorFromData.cs
@@ -5,58 +5,68 @@ using UnityEngine;
 
 public class CustomUnitGeneratorFromData : MonoBehaviour, IUnitGenerator {
 
+    [Header("Elements")]
     [SerializeField]
     private Transform UnitsParent;
     [SerializeField]
     private Transform CellsParent;
     [SerializeField]
-    private UnitClassEnumToPrefab unitClassToPrefab;
+    private List<Transform> _player1SpawnPoints = null;
+    [SerializeField]
+    private List<Transform> _player2SpawnPoints = null;
+    [Space]
+    [Header("Settings")]
+    [SerializeField]
+    private UnitClassEnumToPrefab unitClassToPrefab = null;
 
     private DataController _data;
 
 
 
     private void Start() {
-        
+        Debug.Assert(_player1SpawnPoints.Count == PickMenuController.maxUnits, "Not all spawnpoints have been set");
+        Debug.Assert(_player2SpawnPoints.Count == PickMenuController.maxUnits, "Not all spawnpoints have been set");
     }
 
     public List<Unit> SpawnUnits(List<Cell> cells) {
-        List<Unit> ret = new List<Unit>();
-        for (int i = 0; i < UnitsParent.childCount; i++) {
-            var unit = UnitsParent.GetChild(i).GetComponent<Unit>();
-            if (unit != null) {
-                var cell = cells.OrderBy(h => Math.Abs((h.transform.position - unit.transform.position).magnitude)).First();
-                if (!cell.IsTaken) {
-                    cell.IsTaken = true;
-                    unit.Cell = cell;
-                    unit.transform.position = cell.transform.position;
-                    unit.Initialize();
-                    ret.Add(unit);
-                }//Unit gets snapped to the nearest cell
-                else {
-                    Destroy(unit.gameObject);
-                }//If the nearest cell is taken, the unit gets destroyed.
-            } else {
-                Debug.LogError("Invalid object in Units Parent game object");
-            }
+        _data = FindObjectOfType<DataController>();
+        Debug.Assert(_data.playersSelection[0].playerUnits.Length == PickMenuController.maxUnits, "Unit picket by player one are not in correct number");
+        Debug.Assert(_data.playersSelection[1].playerUnits.Length == PickMenuController.maxUnits, "Unit picket by player one are not in correct number");
 
+        List<Unit> ret = new List<Unit>();
+
+        for (int i = 0; i < _data.playersSelection[0].playerUnits.Length; i++) {
+            GameObject unitInstance = Instantiate(unitClassToPrefab.enumsToPrefab.Find(u => u.unitClass == _data.playersSelection[0].playerUnits[i]).unitPrefab, _player1SpawnPoints[i].position, _player1SpawnPoints[i].rotation) as GameObject;
+            Unit unit = PositionateUnitInGrid(cells, unitInstance);
+            unit.PlayerNumber = 0;
+            ret.Add(unit);
         }
+
+        for (int i = 0; i < _data.playersSelection[0].playerUnits.Length; i++) {
+            GameObject unitInstance = Instantiate(unitClassToPrefab.enumsToPrefab.Find(u => u.unitClass == _data.playersSelection[0].playerUnits[i]).unitPrefab, _player2SpawnPoints[i].position, _player2SpawnPoints[i].rotation) as GameObject;
+            Unit unit = PositionateUnitInGrid(cells, unitInstance);
+            unit.PlayerNumber = 1;
+            ret.Add(unit);
+        }
+
         return ret;
     }
 
-    public void SnapToGrid() {
-        List<Transform> cells = new List<Transform>();
-
-        foreach (Transform cell in CellsParent) {
-            cells.Add(cell);
+    private Unit PositionateUnitInGrid(List<Cell> cells, GameObject unitInstance) {
+        unitInstance.transform.SetParent(UnitsParent);
+        Unit unit = unitInstance.GetComponent<Unit>();
+        if (unit != null) {
+            var cell = cells.OrderBy(h => Math.Abs((h.transform.position - unit.transform.position).magnitude)).First();
+            if (!cell.IsTaken) {
+                cell.IsTaken = true;
+                unit.Cell = cell;
+                unit.transform.position = cell.transform.position;
+                unit.Initialize();
+            }
+        } else {
+            Debug.LogError("Invalid object in Units Parent game object");
         }
 
-        foreach (Transform unit in UnitsParent) {
-            var closestCell = cells.OrderBy(h => Math.Abs((h.transform.position - unit.transform.position).magnitude)).First();
-            if (!closestCell.GetComponent<Cell>().IsTaken) {
-                Vector3 offset = new Vector3(0, closestCell.GetComponent<Cell>().GetCellDimensions().y, 0);
-                unit.localPosition = closestCell.transform.localPosition + offset;
-            }//Unit gets snapped to the nearest cell
-        }
+        return unit;
     }
 }
