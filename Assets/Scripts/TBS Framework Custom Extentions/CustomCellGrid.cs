7d6ae36941a89e420385d5a25c8ddf55d0ec984f
Davide Wylter Di Bernardo <dbr.davide@gmail.com>
2019-04-13 16:41:18 +0200

    -fixed double turn skip

--- Assets/Scripts/TBS Framework Custom Extentions/CustomCellGrid.cs
+++ Assets/Scripts/TBS Framework Custom Extentions/CustomCellGrid.cs
@@ -8,17 +8,24 @@ public class CustomCellGrid : CellGrid{
 
     public bool isUnitPlaying = false;
 
+    private bool _isTurnEnding = false;
+
     protected override void Initialize() {
         base.Initialize();
         GetComponent<CustomObstaclesGenerator>()?.SpawnObstacles();
     }
 
     public void EndTurn(CustomUnit unit) {
-        StartCoroutine(WaitForUnitBeforeSkippingTurn(unit));
+        //If turn is already ending no need to skip again
+        if (_isTurnEnding) {
+            StartCoroutine(WaitForUnitBeforeSkippingTurn(unit));
+        }
     }
 
 
     private IEnumerator WaitForUnitBeforeSkippingTurn(CustomUnit unit) {
+        _isTurnEnding = true;
+
         while (unit.isActing) {
             yield return null;
         }
@@ -31,5 +38,6 @@ public class CustomCellGrid : CellGrid{
         }
 
         EndTurn();
+        _isTurnEnding = false;
     }
 }
